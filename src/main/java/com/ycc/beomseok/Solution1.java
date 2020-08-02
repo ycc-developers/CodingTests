a4cd12f56095db3f1653617fbb6305aaf5b0c3f6
장범석/모바일서비스개발랩/NE <beomseok.jang@nhnent.com>
2020-08-02 21:29:14 +0900

    refactor - 솔루션클래스를 코틀린으로 변경

--- src/main/java/com/ycc/beomseok/Solution1.java
+++
@@ -1,7 +0,0 @@
-package com.ycc.beomseok;
-
-public class Solution1 {
-    public int solve(String compressed) {
-        return 0;
-    }
-}
---
+++ src/main/java/com/ycc/beomseok/Solution1.kt
@@ -0,0 +1,7 @@
+package com.ycc.beomseok
+
+class Solution1 {
+    fun solve(compressed: String): Int {
+        return 0
+    }
+}
