9cf9afa5c36603742973b5e65dad6a4b0acaee5e
장범석/모바일서비스개발랩/NE <beomseok.jang@nhnent.com>
2020-08-07 22:39:09 +0900

    feat - 솔루션 코드 작성

--- src/main/java/com/ycc/beomseok/Solution1.kt
+++ src/main/java/com/ycc/beomseok/Solution1.kt
@@ -2,6 +2,34 @@ package com.ycc.beomseok
 
 class Solution1 {
     fun solve(compressed: String): Int {
-        return 0
+        val compressedResult = mutableListOf<Int>()
+        val maxWrapWordCount = compressed.length / 2
+        for (i in maxWrapWordCount downTo 1) {
+            val compress = compress(compressed, i)
+            compressedResult += compress
+        }
+        return compressedResult.min()?: compressed.length
+    }
+
+    private fun compress(src: String, wrapWordCount: Int): Int {
+        val wordArray = mutableListOf<String>()
+        val wordCounterArray = mutableListOf<Int>()
+        for (i in src.indices step wrapWordCount) {
+            val endIndex = (i + wrapWordCount).coerceAtMost(src.length)
+            val wrapWord = src.substring(i, endIndex)
+            if (wordArray.isNotEmpty() && wordArray[wordArray.size - 1] == wrapWord) {
+                wordCounterArray[wordArray.size - 1]++
+            } else {
+                wordArray.add(wrapWord)
+                wordCounterArray.add(1)
+            }
+        }
+        var result = 0
+        for ((index, word) in wordArray.withIndex()) {
+            val entryCount = if (wordCounterArray[index] == 1) "" else "${wordCounterArray[index]}"
+            val wordCount = "$entryCount${word}".length
+            result += wordCount
+        }
+        return result
     }
 }
