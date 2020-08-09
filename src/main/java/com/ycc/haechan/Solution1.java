ec7df8e8675b572cde09d515ca80c66f43bff029
johaechan <68118083+johaechan@users.noreply.github.com>
2020-08-09 21:00:56 +0900

    솔루션 코드 작성

--- src/main/java/com/ycc/haechan/Solution1.java
+++ src/main/java/com/ycc/haechan/Solution1.java
@@ -1,10 +1,43 @@
 package com.ycc.haechan;
 
+
 public class Solution1 {
-    public String solve(String compressed) {
-        return "";
-    }
-    public int solve(String compressed){
-        return 0;
+
+    public int solve(String s){
+        int answer = s.length();
+
+        for (int i = 1; i <= s.length() / 2; i++) {
+
+            StringBuilder result = new StringBuilder();
+            String presentValue = s.substring(0, i);
+            int count = 1;
+
+            for (int j = i; j < s.length(); j += i) {
+                int len = Math.min(j + i, s.length());
+                String substring = s.substring(j, len);
+                if (presentValue.equals(substring)) {
+                    count++;
+                } else {
+                    if (count == 1) {
+                        result.append(presentValue);
+                    } else {
+                        result.append(count).append(presentValue);
+                    }
+                    presentValue = substring;
+                    count = 1;
+                }
+            }
+
+            if (count != 1) {
+                result.append(count).append(presentValue);
+            } else {
+                result.append(presentValue);
+            }
+
+            answer = Math.min(result.length(), answer);
+
+        }
+
+        return answer;
     }
 }
--- src/test/java/com/ycc/haechan/Solution1Tests.java
+++ src/test/java/com/ycc/haechan/Solution1Tests.java
@@ -1,6 +1,6 @@
 package com.ycc.haechan;
 
-import com.ycc.beomseok.Solution1;
+import com.ycc.haechan.Solution1;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,20 +11,6 @@ public class Solution1Tests {
     @BeforeEach
     public void prepareTest() {
         solution1 = new Solution1();
-    }
-
-    @Test
-    public void given_case1_then_succes() {
-        //GIVEN - 테스트케이스, 결과기댓값 준비
-        String preparedTestValue = "TestValue";
-        String expectedResultValue = "";
-
-        //WHEN - 실제 로직 수행
-        String actualResultValue = solution1.solve(preparedTestValue);
-        System.out.println("test : " + actualResultValue);
-
-        //THEN - 결과 비교
-        Assertions.assertEquals(expectedResultValue, actualResultValue);
     }
 
     @Test
@@ -68,6 +54,7 @@ public class Solution1Tests {
         //THEN - 결과 비교
         Assertions.assertEquals(expectedResultValue, actualResultValue);
     }
+
     @Test
     public void abcabcabcabcdededededede_then_14() {
         //GIVEN - 테스트케이스, 결과기댓값 준비
@@ -113,7 +100,7 @@ public class Solution1Tests {
     @Test
     public void abcabcxdedede_then_11() {
         //GIVEN - 테스트케이스, 결과기댓값 준비
-        String preparedTestValue = "ababcdcdcdcdcdefge";
+        String preparedTestValue = "abcabcxdedede";
         int expectedResultValue = 11;
 
         //WHEN - 실제 로직 수행
@@ -165,19 +152,5 @@ public class Solution1Tests {
         //THEN - 결과 비교
         Assertions.assertEquals(expectedResultValue, actualResultValue);
     }
-
-
-    @Test
-    public void given_case2_then_fail() {
-        //GIVEN - 테스트케이스, 결과기댓값 준비
-        String preparedTestValue = "TestValue";
-        String expectedResultValue = ";";
-
-        //WHEN - 실제 로직 수행
-        String actualResultValue = solution1.solve(preparedTestValue);
-        System.out.println("test : " + actualResultValue);
-
-        //THEN - 결과 비교
-        Assertions.assertNotEquals(expectedResultValue, actualResultValue);
-    }
 }
+
